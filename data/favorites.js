3c785c58d3a88e4a7fcadbb4bad67c3f0868b78e
bwapes <benwaples@gmail.com>
2020-08-26 09:45:50 -0700

    fix unique biz_id bug

--- data/favorites.js
+++ data/favorites.js
@@ -1,16 +1,16 @@
 module.exports = [
   {
-    biz_id: 'YaUGYnfBjq1V-sI0BSokSQ',
-    title: 'Bikini Bottoms Coffee Shop',
+    biz_id: 'YaUGYnfBjq1V-sI0BSokSQ-uniqueId',
+    title: 'Coffee Shop',
     img: 'http://placekitten.com/200/300',
     address: 'asdfsdf',
     is_closed: false,
     rating: 5.0,
-    notes: 'blah blah blah',
+    notes: 'your notes go here',
     yelp_url: 'https://www.yelp.com/biz/ovation-coffee-and-tea-portland?adjust_creative=1kvmc9F-97bT0bHKFMMEUQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=1kvmc9F-97bT0bHKFMMEUQ',
-    lat: 'jfjfjfj',
-    lon: 'jfjfjf',
-    city_lat: 'jfjfj',
-    city_lon: 'jfj'
+    lat: 'string',
+    lon: 'string',
+    city_lat: 'string',
+    city_lon: 'string'
   }
 ];
